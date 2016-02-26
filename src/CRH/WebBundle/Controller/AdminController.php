09fede4f2572f047683162f292574318cf859656
noahj2 <noahj2@winthrop.edu>
2016-02-26 21:11:44 +0000

    Added Controller and security parameters to test HTTP basic auth for admin pages.

---
+++ src/CRH/WebBundle/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+//Custom Admin Controller
+
+namespace CRH\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
+    }
+}
+}
