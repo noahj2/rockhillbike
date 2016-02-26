8e93dd798204f059d33d906f74985241f69d4947
noahj2 <noahj2@winthrop.edu>
2016-02-26 16:30:10 +0000

    Checkpount

--- src/CRH/WebBundle/Entity/Trail.php
+++ src/CRH/WebBundle/Entity/Trail.php
@@ -19,7 +19,6 @@ class Trail
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
-     * 
      */
     private $id;
 
---
+++ src/CRH/WebBundle/Form/AnnouncementType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CRH\WebBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnouncementType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('message')
+            ->add('isCurrent')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CRH\WebBundle\Entity\Announcement'
+        ));
+    }
+}
